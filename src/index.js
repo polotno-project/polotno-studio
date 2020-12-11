da0cef062f4a2970ffd9c81e07dbf973a786fbda
Anton Lavrenov <lavrton@gmail.com>
2020-12-11 13:31:14 -0500

    update viewport

--- src/index.js
+++ src/index.js
@@ -29,8 +29,13 @@ ReactDOM.render(
 );
 
 if (window.innerWidth < 650) {
+  var mvp = document.getElementById('__viewport');
+  mvp.setAttribute(
+    'content',
+    'width=device-width, height=device-height, initial-scale=0.5'
+  );
   alert(
-    'Hey, looks like you opened the app from the mobile. Polotno Studio is not optimized yet for mobile. But thanks for your interest! We will have mobile support soon. For now, please use it on the desktop.'
+    'Hey, looks like you opened the app from the mobile. Polotno Studio is not optimized for mobile yet. But thanks for your interest! We will have mobile support soon. For now, please use it on the desktop.'
   );
 }
 
