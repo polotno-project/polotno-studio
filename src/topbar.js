2b07581f81a8ce7007e8151d06ce1c63f168bc21
Anton Lavrenov <lavrton@gmail.com>
2022-05-06 12:53:23 -0500

    new export button

--- src/topbar.js
+++ src/topbar.js
@@ -10,12 +10,14 @@ import {
   Classes,
   Position,
   Menu,
-  MenuItem,
+  HTMLSelect,
+  Slider,
 } from '@blueprintjs/core';
 import FaGithub from '@meronex/icons/fa/FaGithub';
 import FaDiscord from '@meronex/icons/fa/FaDiscord';
 import { downloadFile } from 'polotno/utils/download';
 import { Popover2 } from '@blueprintjs/popover2';
+import * as unit from 'polotno/utils/unit';
 import { t } from 'polotno/utils/l10n';
 
 import styled from 'polotno/utils/styled';
@@ -34,8 +36,10 @@ const NavInner = styled('div')`
   }
 `;
 
-const DownloadButton = ({ store }) => {
+const DownloadButton = observer(({ store }) => {
   const [saving, setSaving] = React.useState(false);
+  const [quality, setQuality] = React.useState(1);
+  const [type, setType] = React.useState('png');
 
   const getName = () => {
     const texts = [];
@@ -54,7 +58,94 @@ const DownloadButton = ({ store }) => {
     <Popover2
       content={
         <Menu>
-          <MenuItem
+          <li class="bp4-menu-header">
+            <h6 class="bp4-heading">File type</h6>
+          </li>
+          <HTMLSelect
+            fill
+            onChange={(e) => {
+              setType(e.target.value);
+              setQuality(1);
+            }}
+            value={type}
+          >
+            <option value="jpeg">JPEG</option>
+            <option value="png">PNG</option>
+            <option value="pdf">PDF</option>
+          </HTMLSelect>
+          <li class="bp4-menu-header">
+            <h6 class="bp4-heading">Size</h6>
+          </li>
+          <div style={{ padding: '10px' }}>
+            <Slider
+              value={quality}
+              labelRenderer={false}
+              // labelStepSize={0.4}
+              onChange={(quality) => {
+                setQuality(quality);
+              }}
+              stepSize={0.2}
+              min={0.2}
+              max={3}
+              showTrackFill={false}
+            />
+            {type === 'pdf' && (
+              <div>
+                {unit.pxToUnitRounded({
+                  px: store.width,
+                  dpi: store.dpi / quality,
+                  precious: 0,
+                  unit: 'mm',
+                })}{' '}
+                x{' '}
+                {unit.pxToUnitRounded({
+                  px: store.height,
+                  dpi: store.dpi / quality,
+                  precious: 0,
+                  unit: 'mm',
+                })}{' '}
+                mm
+              </div>
+            )}
+            {type !== 'pdf' && (
+              <div>
+                {Math.round(store.width * quality)} x{' '}
+                {Math.round(store.height * quality)} px
+              </div>
+            )}
+          </div>
+          <Button
+            fill
+            intent="primary"
+            loading={saving}
+            onClick={async () => {
+              if (type === 'pdf') {
+                setSaving(true);
+                await store.saveAsPDF({
+                  fileName: getName() + '.pdf',
+                  dpi: store.dpi / quality,
+                  pixelRatio: 2 * quality,
+                });
+                setSaving(false);
+              } else {
+                store.pages.forEach((page, index) => {
+                  // do not add index if we have just one page
+                  const indexString =
+                    store.pages.length > 1 ? '-' + (index + 1) : '';
+                  store.saveAsImage({
+                    pageId: page.id,
+                    pixelRatio: quality,
+                    mimeType: 'image/' + type,
+                    fileName: getName() + indexString + '.' + type,
+                  });
+                });
+              }
+            }}
+          >
+            Download {type.toUpperCase()}
+          </Button>
+
+          {/* <MenuItem
             icon="media"
             text={t('toolbar.saveAsImage')}
             onClick={async () => {
@@ -79,20 +170,23 @@ const DownloadButton = ({ store }) => {
               });
               setSaving(false);
             }}
-          />
+          /> */}
         </Menu>
       }
-      position={Position.BOTTOM}
+      position={Position.BOTTOM_RIGHT}
     >
       <Button
         icon="import"
         text={t('toolbar.download')}
-        minimal
+        intent="primary"
         loading={saving}
+        onClick={() => {
+          setQuality(1);
+        }}
       />
     </Popover2>
   );
-};
+});
 
 export default observer(({ store }) => {
   const inputRef = React.useRef();
