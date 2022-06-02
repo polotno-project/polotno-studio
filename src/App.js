d09f15a7c0eacbe781b045080cdbb0a759d7027a
Anton Lavrenov <lavrton@gmail.com>
2022-06-01 20:39:34 -0500

    remove icons

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Topbar from './topbar';
 
 // hide it for now
 // DEFAULT_SECTIONS.splice(3, 0, IllustrationsSection);
-DEFAULT_SECTIONS.splice(3, 0, FlaticonSection);
+// DEFAULT_SECTIONS.splice(3, 0, FlaticonSection);
 
 const useHeight = () => {
   const [height, setHeight] = React.useState(window.innerHeight);
