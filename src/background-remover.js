c1d6087bfdd988183ebdf0afa077e8a002047271
Anton Lavrenov <lavrton@gmail.com>
2023-11-08 09:20:03 -0500

    fix url

--- src/background-remover.js
+++ src/background-remover.js
@@ -9,8 +9,7 @@ import { useProject } from './project';
 
 let removeBackgroundFunc = async (url) => {
   const req = await fetch(
-    'https://api.polotno.com/api/remove-image-background-replicate?KEY=' +
-      getKey(),
+    'https://api.polotno.com/api/remove-image-background?KEY=' + getKey(),
     {
       method: 'POST',
       headers: {
