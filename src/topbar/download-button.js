7e016e1b8686941f981fc8386746fd24b2b1cd15
Anton Lavrenov <lavrton@gmail.com>
2024-01-26 10:59:37 -0500

    fix button

--- src/topbar/download-button.js
+++ src/topbar/download-button.js
@@ -155,69 +155,71 @@ export const DownloadButton = observer(({ store }) => {
             intent="primary"
             loading={saving}
             onClick={async () => {
-              if (type === 'pdf') {
-                setSaving(true);
-                await store.saveAsPDF({
-                  fileName: getName() + '.pdf',
-                  dpi: store.dpi / pageSizeModifier,
-                  pixelRatio: 2 * quality,
-                });
-                setSaving(false);
-              } else if (type === 'html') {
-                setSaving(true);
-                await store.saveAsHTML({
-                  fileName: getName() + '.html',
-                });
-                setSaving(false);
-              } else if (type === 'gif') {
-                setSaving(true);
-                await store.saveAsGIF({
-                  fileName: getName() + '.gif',
-                  pixelRatio: quality,
-                  fps,
-                });
-                setSaving(false);
-              } else {
-                setSaving(true);
-                if (store.pages.length < 3) {
-                  store.pages.forEach((page, index) => {
-                    // do not add index if we have just one page
-                    const indexString =
-                      store.pages.length > 1 ? '-' + (index + 1) : '';
-                    store.saveAsImage({
-                      pageId: page.id,
-                      pixelRatio: quality,
-                      mimeType: 'image/' + type,
-                      fileName: getName() + indexString + '.' + type,
-                    });
+              setSaving(true);
+              try {
+                if (type === 'pdf') {
+                  await store.saveAsPDF({
+                    fileName: getName() + '.pdf',
+                    dpi: store.dpi / pageSizeModifier,
+                    pixelRatio: 2 * quality,
+                  });
+                } else if (type === 'html') {
+                  await store.saveAsHTML({
+                    fileName: getName() + '.html',
+                  });
+                } else if (type === 'gif') {
+                  await store.saveAsGIF({
+                    fileName: getName() + '.gif',
+                    pixelRatio: quality,
+                    fps,
                   });
                 } else {
-                  const zip = new JSZip();
-                  for (const page of store.pages) {
-                    const index = store.pages.indexOf(page);
-                    const indexString =
-                      store.pages.length > 1 ? '-' + (index + 1) : '';
-
-                    const url = await store.toDataURL({
-                      pageId: page.id,
-                      pixelRatio: quality,
-                      mimeType: 'image/' + type,
+                  if (store.pages.length < 3) {
+                    store.pages.forEach((page, index) => {
+                      // do not add index if we have just one page
+                      const indexString =
+                        store.pages.length > 1 ? '-' + (index + 1) : '';
+                      store.saveAsImage({
+                        pageId: page.id,
+                        pixelRatio: quality,
+                        mimeType: 'image/' + type,
+                        fileName: getName() + indexString + '.' + type,
+                      });
                     });
-                    const fileName = getName() + indexString + '.' + type;
-                    const base64Data = url.replace(
-                      /^data:image\/(png|jpeg);base64,/,
-                      ''
-                    );
-                    zip.file(fileName, base64Data, { base64: true });
-                  }
+                  } else {
+                    const zip = new JSZip();
+                    for (const page of store.pages) {
+                      const index = store.pages.indexOf(page);
+                      const indexString =
+                        store.pages.length > 1 ? '-' + (index + 1) : '';
 
-                  const content = await zip.generateAsync({ type: 'base64' });
-                  const result = 'data:application/zip;base64,' + content;
-                  console.log(content);
-                  downloadFile(result, getName() + '.zip');
+                      const url = await store.toDataURL({
+                        pageId: page.id,
+                        pixelRatio: quality,
+                        mimeType: 'image/' + type,
+                      });
+                      const fileName = getName() + indexString + '.' + type;
+                      const base64Data = url.replace(
+                        /^data:image\/(png|jpeg);base64,/,
+                        ''
+                      );
+                      zip.file(fileName, base64Data, { base64: true });
+                    }
+
+                    const content = await zip.generateAsync({ type: 'base64' });
+                    const result = 'data:application/zip;base64,' + content;
+                    console.log(content);
+                    downloadFile(result, getName() + '.zip');
+                  }
                 }
-                setSaving(false);
+              } catch (e) {
+                // throw into global error handler for reporting
+                setTimeout(() => {
+                  throw e;
+                });
+                alert('Something went wrong. Please try again.');
               }
+              setSaving(false);
             }}
           >
             Download {type.toUpperCase()}
