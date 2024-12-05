eb3108be11dfe9c0de9e0bd9b90da9cc0d4faacb
Anton Lavrevov <lavrton@gmail.com>
2024-12-05 08:36:12 -0500

    add source maps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "polotno",
+    project: "polotno-studio"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
