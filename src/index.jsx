5f410844d8e0a37713a24ea65a29d631441fd12e
Anton Lavrevov <lavrton@gmail.com>
2025-06-24 06:12:37 -0500

    restore

--- src/index.jsx
+++ src/index.jsx
@@ -1,77 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Button } from '@blueprintjs/core';
-import { Office } from '@blueprintjs/icons';
+import { createStore } from 'polotno/model/store';
+import { unstable_setAnimationsEnabled } from 'polotno/config';
+import { createProject, ProjectContext } from './project';
 
-console.log(React, Button, Office);
+import './index.css';
+import App from './App';
+import './logger';
+import { ErrorBoundary } from 'react-error-boundary';
 
-// import { createStore } from 'polotno/model/store';
-// import { unstable_setAnimationsEnabled } from 'polotno/config';
-// import { createProject, ProjectContext } from './project';
+if (window.location.host !== 'studio.polotno.com') {
+  console.log(
+    `%cWelcome to Polotno Studio! Thanks for your interest in the project!
+This repository has many customizations from the default version Polotno SDK.
+I don't recommend to use it as starting point.
+Instead, you can start from any official demos, e.g.: https://polotno.com/docs/full-canvas-editor
+or direct sandbox: https://codesandbox.io/s/github/polotno-project/polotno-site/tree/source/examples/polotno-demo?from-embed.
+But feel free to use this repository as a reference for your own project and to learn how to use Polotno SDK.`,
+    'background: rgba(54, 213, 67, 1); color: white; padding: 5px;'
+  );
+}
 
-// import './index.css';
-// import App from './App';
-// import './logger';
-// import { ErrorBoundary } from 'react-error-boundary';
+unstable_setAnimationsEnabled(true);
 
-// if (window.location.host !== 'studio.polotno.com') {
-//   console.log(
-//     `%cWelcome to Polotno Studio! Thanks for your interest in the project!
-// This repository has many customizations from the default version Polotno SDK.
-// I don't recommend to use it as starting point.
-// Instead, you can start from any official demos, e.g.: https://polotno.com/docs/full-canvas-editor
-// or direct sandbox: https://codesandbox.io/s/github/polotno-project/polotno-site/tree/source/examples/polotno-demo?from-embed.
-// But feel free to use this repository as a reference for your own project and to learn how to use Polotno SDK.`,
-//     'background: rgba(54, 213, 67, 1); color: white; padding: 5px;'
-//   );
-// }
+const store = createStore({ key: 'nFA5H9elEytDyPyvKL7T' });
+window.store = store;
+store.addPage();
 
-// unstable_setAnimationsEnabled(true);
+const project = createProject({ store });
+window.project = project;
 
-// const store = createStore({ key: 'nFA5H9elEytDyPyvKL7T' });
-// window.store = store;
-// store.addPage();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// const project = createProject({ store });
-// window.project = project;
+function Fallback({ error, resetErrorBoundary }) {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <div style={{ textAlign: 'center', paddingTop: '40px' }}>
+        <p>Something went wrong in the app.</p>
+        <p>Try to reload the page.</p>
+        <p>If it does not work, clear cache and reload.</p>
+        <button
+          onClick={async () => {
+            await project.clear();
+            window.location.reload();
+          }}
+        >
+          Clear cache and reload
+        </button>
+      </div>
+    </div>
+  );
+}
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// function Fallback({ error, resetErrorBoundary }) {
-//   return (
-//     <div style={{ width: '100vw', height: '100vh' }}>
-//       <div style={{ textAlign: 'center', paddingTop: '40px' }}>
-//         <p>Something went wrong in the app.</p>
-//         <p>Try to reload the page.</p>
-//         <p>If it does not work, clear cache and reload.</p>
-//         <button
-//           onClick={async () => {
-//             await project.clear();
-//             window.location.reload();
-//           }}
-//         >
-//           Clear cache and reload
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// root.render(
-//   <ErrorBoundary
-//     FallbackComponent={Fallback}
-//     onReset={(details) => {
-//       // Reset the state of your app so the error doesn't happen again
-//     }}
-//     onError={(e) => {
-//       if (window.Sentry) {
-//         window.Sentry.captureException(e);
-//       }
-//     }}
-//   >
-//     <ProjectContext.Provider value={project}>
-//       <App store={store} />
-//     </ProjectContext.Provider>
-//   </ErrorBoundary>
-// );
+root.render(
+  <ErrorBoundary
+    FallbackComponent={Fallback}
+    onReset={(details) => {
+      // Reset the state of your app so the error doesn't happen again
+    }}
+    onError={(e) => {
+      if (window.Sentry) {
+        window.Sentry.captureException(e);
+      }
+    }}
+  >
+    <ProjectContext.Provider value={project}>
+      <App store={store} />
+    </ProjectContext.Provider>
+  </ErrorBoundary>
+);
