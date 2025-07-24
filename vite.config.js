d6e9ceddede61175e4a5920200b7e561c2f8a1f9
Anton Lavrevov <lavrton@gmail.com>
2025-07-24 08:22:46 -0500

    install polotno 2.25.5

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,8 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import analyzer from 'vite-bundle-analyzer';
-import path from 'path';
-// https://vite.dev/config/
+
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     // '@blueprintjs/icons': path.resolve('./emptyIconPaths.js'),
-  //     '@blueprintjs/icons/lib/esm/generated': path.resolve(
-  //       './emptyIconPaths.js'
-  //     ),
-  //   },
-  // },
   plugins: [
     react(),
     sentryVitePlugin({
